fc06fa70ec890ab86dbdd59906bc90adfdff02e4
zhangda <zhangda@aerozhonghuan.com>
2020-05-19 11:14:27 +0800

    add

--- common-utils/src/main/java/com/da/learn/utils/singleton/Singleton6.java
+++ common-utils/src/main/java/com/da/learn/utils/singleton/Singleton6.java
@@ -3,6 +3,11 @@ package com.da.learn.utils.singleton;
 /**
  * 静态内部类式
  * 延迟加在  避免线程不安全  同双重检查模式
+ *
+ * JVM在类的初始化阶段（即在Class被加载后，且被线程使用之前），会执行类的初始化。在
+ * 执行类的初始化期间，JVM会去获取一个锁。这个锁可以同步多个线程对同一个类的初始化。
+ *
+ * Initialization On Demand Holder idiom
  */
 public class Singleton6 {
     private Singleton6() {
