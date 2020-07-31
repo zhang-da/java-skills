92a33c8d302cb1993d89ba94640185448473bfdc
zhangda <zhangda@aerozhonghuan.com>
2020-07-31 09:39:38 +0800

    without spring-boot parent

---
+++ other-test-boot/src/main/resources/static/video/02_device/js/client.js
@@ -0,0 +1,38 @@
+'use strict'
+var audioSource  = document.querySelector("select#audioSource");
+var audioOutput  = document.querySelector("select#audioOutput");
+var videoSource  = document.querySelector("select#videoSource");
+
+if(!navigator.mediaDevices ||
+    !navigator.mediaDevices.enumerateDevices){
+    console.log('enumerateDevices is not supported!');
+}else {
+    navigator.mediaDevices.enumerateDevices()
+        .then(gotDevices)
+        .catch(handleError);
+}
+
+function gotDevices(deviceInfos){
+    console.log("整体打印：{}", deviceInfos);
+    deviceInfos.forEach( function(deviceInfo){
+        console.log(deviceInfo.kind + ": label = "
+            + deviceInfo.label + ": id = "
+            + deviceInfo.deviceId + ": groupId = "
+            + deviceInfo.groupId);
+        var option = document.createElement('option');
+        option.text = deviceInfo.label === null || deviceInfo.label === undefined || deviceInfo.label === "" ? "unname" : deviceInfo.label;
+        option.value = deviceInfo.deviceId;
+        if(deviceInfo.kind === 'audioinput'){
+            audioSource.appendChild(option);
+        }else if(deviceInfo.kind === 'audiooutput'){
+            audioOutput.appendChild(option);
+        }else if(deviceInfo.kind === 'videoinput'){
+            videoSource.appendChild(option);
+        }
+    });
+
+}
+
+function handleError(err){
+    console.log(err.name + " : " + err.message);
+}
